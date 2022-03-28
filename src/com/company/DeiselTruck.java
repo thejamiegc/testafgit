d32f9cff2b9a2a579c7e91ff8a3610a69365ede6
thejamiegc <jcallan32@gmail.com>
2022-03-28 11:30:03 +0200

    Diesel truck class and constructor

---
+++ src/com/company/DeiselTruck.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class DeiselTruck extends DieselCar{
+    public DeiselTruck(String registrationNumber,String make, String model, int numberOfDoors, int kmPrLitre,boolean particleFilter){
+        super(registrationNumber, make, model,numberOfDoors, kmPrLitre,particleFilter);
+    }
+}
